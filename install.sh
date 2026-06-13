adee4627d517f2936eab38da9603080423f0fc8e
DalaisonTheBone <141103320+DalisonTheBone@users.noreply.github.com>
2026-06-13 01:26:05 +0000

    fixed paths
    should work to install on system d with no configuration

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo mkdir -p /etc/unifand
-sudo cp ./unifan.conf /etc/unifand
+sudo cp ./unifand.conf /etc/unifand
 
 sudo gcc -Wall -Wextra -o /bin/unifand ./unifand.c
-sudo gcc -Wall -Wextra -o /etc/unifand/module ./thinkpad-universal.c
+sudo gcc -Wall -Wextra -o /etc/unifand/module ./modules/thinkpad-universal.c
 
 sudo cp ./unifand.service /etc/systemd/system/
 sudo systemctl daemon-reload
