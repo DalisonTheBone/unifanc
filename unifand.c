f6e8fbe96d15e2aa174b16089f329887e6df9c65
DalisonTheBone <141103320+DalisonTheBone@users.noreply.github.com>
2026-06-21 11:28:10 -0230

    push

--- unifand.c
+++ unifand.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include "modules/file.c"
+#include "modules/modules.c"
 
 int main(void) {
     //file test = read_file("/etc/unifand/unifand.conf");
+    cmd_output responce = run_command("echo Hello World!");
 
+    printf("%s", responce.bytes);
     
-    printf("%d", is_valid_path("/etc/unifand/"));
 }
